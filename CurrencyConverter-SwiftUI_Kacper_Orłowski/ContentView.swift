9c78717ce197690682824c51b174c49230ff2444
Dubiel <imob@pk.edu.pl>
2025-05-27 13:00:05 +0200

    Initial Commit

---
+++ CurrencyConverter-SwiftUI_Kacper_Orłowski/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CurrencyConverter-SwiftUI_Kacper_Orłowski
+//
+//  Created by iMOB05 on 27/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CurrencyConverter-SwiftUI_Kacper_Orłowski/CurrencyConverter_SwiftUI_Kacper_Or_owskiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverter_SwiftUI_Kacper_Or_owskiApp.swift
+//  CurrencyConverter-SwiftUI_Kacper_Orłowski
+//
+//  Created by iMOB05 on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverter_SwiftUI_Kacper_Or_owskiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
